6699e9f8f7e0e12df72969ca3ae3f0bd9540a3a8
bz866 <bz866@nyu.edu>
2021-08-31 22:11:31 +0100

    mybatis mapper

--- src/main/java/com/jiuzhang/seckill/controller/EchoController.java
+++ src/main/java/com/jiuzhang/seckill/controller/EchoController.java
@@ -1,4 +1,4 @@
-package com.ericzengproject.lighntingdealecommerce.controller;
+package com.jiuzhang.seckill.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/test/java/com/jiuzhang/seckill/LighntingDealEcommerceApplicationTests.java
+++ src/test/java/com/jiuzhang/seckill/LighntingDealEcommerceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ericzengproject.lighntingdealecommerce;
+package com.jiuzhang.seckill;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
