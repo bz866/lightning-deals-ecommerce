a196d6671c90c627190b2c78918e19168d877e07
bz866 <bz866@nyu.edu>
2021-09-01 10:44:03 +0100

    invertory decrement with buy request

--- src/main/java/com/jiuzhang/seckill/SeckillApplication.java
+++ src/main/java/com/jiuzhang/seckill/SeckillApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @MapperScan("com.jiuzhang.seckill.db.mappers")
 @ComponentScan(basePackages = {"com.jiuzhang"})
-
 public class SeckillApplication {
 
     public static void main(String[] args) {
